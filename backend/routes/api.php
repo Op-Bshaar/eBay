dca86ecc59dcd6320ad1768a39ade823d724d2a5
Op-Bshaar <basharabdalla5@gmail.com>
2024-09-04 19:29:20 +0300

    fdaasdf

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
