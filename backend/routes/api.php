2236d3c2eebc468ec72f84b80711a2d6338e4687
Omar-Salah123 <sd.omar04@gmail.com>
2024-09-04 17:17:17 +0300

    to be continued

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class,'register']);
