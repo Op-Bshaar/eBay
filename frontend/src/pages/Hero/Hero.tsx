9e93614013eaeb4d4ec8bd80e085d0323f384c91
Op-Bshaar <basharabdalla5@gmail.com>
2024-09-05 10:45:44 +0300

    ..

--- frontend/src/pages/Hero/Hero.tsx
+++ frontend/src/pages/Hero/Hero.tsx
@@ -1,73 +1,85 @@
 import React, { useState } from "react";
-import "./Hero.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Button, Menu } from "antd";
 
-function Hero() {
-  const [showmenu, setshowmenu] = useState(false);
-  const [activemenu, setactivemenu] = useState<null | number>(null);
+type MenuItem = Required<MenuProps>["items"][number];
 
-  const handlemouseEnter = () => {
-    setshowmenu(true);
-  };
+const items: MenuItem[] = [
+  { key: "1", icon: <PieChartOutlined />, label: "جوالات" },
+  { key: "2", icon: <DesktopOutlined />, label: "لابتوبات" },
+  { key: "3", icon: <ContainerOutlined />, label: "أجهزة منزليه" },
+  {
+    key: "sub1",
+    label: "اكسسوارات",
+    icon: <MailOutlined />,
+    children: [
+      { key: "5", label: "اي شي" },
+      { key: "6", label: "اي شي" },
+      { key: "7", label: "اي شي" },
+      { key: "8", label: "اي شي" },
+    ],
+  },
+  {
+    key: "sub2",
+    label: "ألعاب فيديو",
+    icon: <AppstoreOutlined />,
+    children: [
+      { key: "9", label: "اي شي" },
+      { key: "10", label: "اي شي" },
+      {
+        key: "sub3",
+        label: "قطع",
+        children: [
+          { key: "11", label: "اي شي" },
+          { key: "12", label: "اي شي" },
+        ],
+      },
+    ],
+  },
+  { key: "13", icon: <PieChartOutlined />, label: "جوالات" },
+  { key: "14", icon: <DesktopOutlined />, label: "لابتوبات" },
+  { key: "15", icon: <ContainerOutlined />, label: "أجهزة منزليه" },
+  { key: "1", icon: <PieChartOutlined />, label: "جوالات" },
+  { key: "2", icon: <DesktopOutlined />, label: "لابتوبات" },
+  { key: "3", icon: <ContainerOutlined />, label: "أجهزة منزليه" },
+  { key: "3", icon: <ContainerOutlined />, label: "أجهزة منزليه" },
+];
 
-  const handlemouseLeave = () => {
-    setshowmenu(false);
-    setactivemenu(null);
-  };
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
 
-  const handlesubmouseEnter = (index: number) => {
-    setactivemenu(index);
-  };
-
-  const handlesubmouseLeave = () => {
-    setactivemenu(null);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
   };
 
   return (
-    <div className="hero-container">
-      <div className="hero-content">
-        <div
-          className="icon-text-style"
-          onMouseEnter={handlemouseEnter}
-          onMouseLeave={handlemouseLeave}
-        >
-          <FontAwesomeIcon icon={faBars} className="hamburger-icon" />
-          <span className="hero-text" style={{ marginRight: 10 }}>
-            التصنيفات
-          </span>
-        </div>
-        {showmenu && (
-          <div className="menu">
-            {["أجهزة منزلية", "لابتوبات", "جوالات"].map((item, index) => (
-              <div
-                key={index}
-                className="menu-item"
-                onMouseEnter={() => handlesubmouseEnter(index)}
-                onMouseLeave={handlesubmouseLeave}
-              >
-                <p>{item}</p>
-                {activemenu === index && (
-                  <div className="sub-menu">
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                    <p>تفاصيل</p>
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+    <div style={{ width: 256 }}>
+      <Button
+        type="primary"
+        onClick={toggleCollapsed}
+        style={{ marginBottom: 16 }}
+      >
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </Button>
+      <Menu
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+        items={items}
+      />
     </div>
   );
-}
+};
 
-export default Hero;
+export default App;
