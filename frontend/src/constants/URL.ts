877bd38c9b7e9cba0eb1d8e45fca0b137f1a03b4
Omer Yassir <omerym10@gmail.com>
2024-09-08 17:50:02 +0300

    sdfdsf

--- frontend/src/constants/URL.ts
+++ frontend/src/constants/URL.ts
@@ -2,6 +2,7 @@ export const BASE_URL = "http://127.0.0.1:8000";
 export const API_URL = `${BASE_URL}/api`;
 export const LOGIN_URL = "/login";
 export const PAGE_URLS = {
+    home:"/",
     search: "/products/search",
     login: "/login",
     register: "/register",
