15f5aed7fe9c843808da0cb827c81b28870a54f7
Omar-Salah123 <sd.omar04@gmail.com>
2024-09-08 14:43:49 +0300

    working on search

--- frontend/src/Product.ts
+++ frontend/src/Product.ts
@@ -1,4 +1,15 @@
 class Product {
-    // TODO: fill
+    public Price: number;
+    public PName: string;
+    public SellerName: string;
+    public Category: string;
+    constructor(Price: number, PName: string, SellerName: string, Category: string)
+    {
+        this.Price = Price;
+        this.PName = PName;
+        this.SellerName = SellerName;
+        this.Category = Category;
+    }
+
 }
 export default Product;
