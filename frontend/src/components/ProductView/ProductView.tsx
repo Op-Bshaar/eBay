cac82c1b390bbcdefc8c1d8bc5a5f45f5549ce27
Omar-Salah123 <sd.omar04@gmail.com>
2024-09-08 17:07:45 +0300

    added css for the products that are displayed upon searching

--- frontend/src/components/ProductView/ProductView.tsx
+++ frontend/src/components/ProductView/ProductView.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Product from '../../Product';
+import './ProductView.css'
 
 function ProductView({ product }: { product: Product }) {
     console.log(product);
     // TODO: Render Product
     return (
-        <div className="product-view">  
+        <div className="product-view"> 
+            <img src= {product.image} alt= {"لا توجد صورة"} className="product-image" />
+            <div className="product-properties"> 
             <h2 className="product-title">{product.title}</h2>
             <p className="product-description">{product.description}</p>
             <p className="product-price">${product.price}</p>
+            </div>
         </div>
     );
 }
