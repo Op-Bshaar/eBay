f2c5d5fac3783dd02f976e6d47605ee07c1a9417
Omar-Salah123 <sd.omar04@gmail.com>
2024-09-08 16:35:14 +0300

    fixed search

--- frontend/src/components/ProductView/ProductView.tsx
+++ frontend/src/components/ProductView/ProductView.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import Product from '../../Product';
 
 function ProductView({ product }: { product: Product }) {
+    console.log(product);
     // TODO: Render Product
     return (
-        <p>{`${product}`}</p>
+        <div className="product-view">  
+            <h2 className="product-title">{product.title}</h2>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">${product.price}</p>
+        </div>
     );
 }
 
