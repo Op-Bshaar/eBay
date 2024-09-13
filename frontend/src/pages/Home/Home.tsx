26031458bf33e9aebf4b8df40447a37ae5e82b4c
Op-Bshaar <basharabdalla5@gmail.com>
2024-09-13 10:35:46 +0300

    helo

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -9,51 +9,55 @@ import "./Home.css";
 import "../ProductsContainer.css";
 import ErrorMessage from "../../components/errorMessage/Error";
 function Home() {
-    const [products, setProducts] = useState<Product[]>([]);
-    const [error, setError] = useState<string>("");
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                setError("");
-                setIsLoading(true);
-                const response = await api.get(`/products`);
-                const data = await response.data;
-                setProducts(data);
-            } catch (error) {
-                console.error("Fetch error:", error);
-                setError("An error occurred while fetching products.");
-            }
-            finally {
-                setIsLoading(false);
-            }
-        };
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setError("");
+        setIsLoading(true);
+        const response = await api.get(`/products`);
+        const data = await response.data;
+        setProducts(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+        setError("An error occurred while fetching products.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-        fetchProducts();
-    }, []);
-    const productsView = error || isLoading ?
-        <div className="tajawal-extralight home-message big-message">
-            {isLoading && <div className="loader" />}
-            {error && <ErrorMessage>معذره حدثت مشكله</ErrorMessage>}
-        </div> :
-        <div className="products-container fill-flex">
-            {products.length > 0 ? (
-                products.map((product, index) => <ProductView key={index} product={product} />)
-            ) : (
-                <p>No products available</p>
-            )}
-        </div>;
-    return (
-        <div className="tajawal-extralight">
-            <div className="topbar-container">
-                <TopBar />
-                <div className="sidebar-container">
-                    <SideBar />
-                    {productsView}
-                </div>
-            </div>
-        </div>
+    fetchProducts();
+  }, []);
+  const productsView =
+    error || isLoading ? (
+      <div className="tajawal-extralight home-message big-message">
+        {isLoading && <div className="loader" />}
+        {error && <ErrorMessage>معذره حدثت مشكله</ErrorMessage>}
+      </div>
+    ) : (
+      <div className="products-container fill-flex">
+        {products.length > 0 ? (
+          products.map((product, index) => (
+            <ProductView key={index} product={product} />
+          ))
+        ) : (
+          <p>No products available</p>
+        )}
+      </div>
     );
+  return (
+    <div className="tajawal-extralight">
+      <div className="topbar-container">
+        <TopBar />
+        <div className="sidebar-container">
+          <SideBar />
+          {productsView}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
