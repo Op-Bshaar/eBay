e5e3b3f62a5ebc3b0a0ca6ee52d95206eaf7e44e
Op-Bshaar <basharabdalla5@gmail.com>
2024-09-13 06:56:47 +0300

    ,,

--- backend/database/migrations/2024_09_12_171417_create_categories_table.php
+++ backend/database/migrations/2024_09_12_171417_create_categories_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string("name")->unique();
-            $table->string("icon");
+            $table->string('name')->unique();
+            $table->string('icon');
             $table->timestamps();
         });
     }
