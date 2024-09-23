5a502fdff7cf05302f0dedcefd14911c8c98e5c5
Omer Yassir <omerym10@gmail.com>
2024-09-23 12:39:52 +0300

    gsfa

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -47,22 +47,17 @@ Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
-
+// Route for seller
 Route::middleware('auth:sanctum')->group(function (){
 Route::get('/sellers/products', [SellerController::class, 'getAllSellersProducts']);
-Route::post('/sellers/products', [SellerController::class, 'addProducts']);
 Route::put('/sellers/products/{id}', [SellerController::class, 'updateProduct']);
 Route::delete('/sellers/products/{id}', [SellerController::class, 'deleteProduct']);
+Route::post('/sellers/products', [SellerController::class, 'addProducts']);
 });
 
 
 //Route for category 
 Route::get('/categories', [CategoryController::class, 'index']);
-
-
-// Route for seller
-
-
 
 // cart Route
 Route::middleware('auth:sanctum')->group(function() {
