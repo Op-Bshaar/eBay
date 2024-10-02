41e2e8b1c3c863706ce924353c61420c9b4ad1f6
Omer Yassir <omerym10@gmail.com>
2024-10-02 15:06:56 +0300

    daf

--- admindashboard/src/Pages/AdminOrder.tsx
+++ admindashboard/src/Pages/AdminOrder.tsx
@@ -1,6 +1,8 @@
 ﻿import { useParams } from "react-router-dom";
 import useOrderItem from "../../../frontend/src/helpers/useOrderItem";
+
 import "./css/AdminOrder.css";
+import { addressToText } from "../../../frontend/src/pages/AddressInput/Address";
 function AdminOrder() {
     const { orderId } = useParams();
     const { order, messageElement } = useOrderItem(orderId);
@@ -8,11 +10,35 @@ function AdminOrder() {
         return <div className="admin-order-message">{messageElement}</div>;
     }
     return (
-        <div className ="admin-order">
+        <div className="admin-order">
             <article>
                 <h2>
                     معلومات الشحن
                 </h2>
+                <div>
+                    <span>
+                        شركة الشحن
+                    </span>
+                    <span>
+                        {order.shipping_company}
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        العنوان
+                    </span>
+                    <span>
+                        {order.order_request && addressToText(order.order_request)}
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        العنوان
+                    </span>
+                    <span>
+                        {order.order_request && addressToText(order.order_request)}
+                    </span>
+                </div>
             </article>
             <article>
                 <h2>
