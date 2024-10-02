92f674c6f064af3c232c86d12168a1756c687c45
Omer Yassir <omerym10@gmail.com>
2024-10-02 17:08:59 +0300

    dfsfd

--- admindashboard/src/Pages/AdminOrder.tsx
+++ admindashboard/src/Pages/AdminOrder.tsx
@@ -9,72 +9,83 @@ function AdminOrder() {
     if (!order) {
         return <div className="admin-order-message">{messageElement}</div>;
     }
+    const cancelled = order.status === 'canceled' || order.status === 'failed' || order.status === 'timeout';
+    const shipped = order.status === 'shipped';
+    const paid = order.status === 'paid' || order.status === 'notified-seller' || order.status === 'shipped';
     return (
-        <div className="center-text">
+        <div className="admin-order-page">
             <div className="admin-order-info">
-            <article>
-                <h2>
-                    معلومات البائع
-                </h2>
-                <div>
-                    {order.product.seller_id }
-                </div>
-            </article>
-            <article>
-                <h2>
-                    معلومات المشتري
-                </h2>
-                <div>
-                    {order.order_request?.user_id }
-                </div>
-            </article>
-            <article>
-                <h2>
-                    معلومات الشحن
-                </h2>
-                <div>
-                    <span>
-                        شركة الشحن: 
-                    </span>
-                    <span>
-                        {order.shipping_company}
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        رقم الشحنة: 
-                    </span>
-                    <span>
-                        {order.shipment_number}
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        العنوان: 
-                    </span>
-                    <span>
-                        {order.order_request && addressToText(order.order_request)}
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        اسم المستلم: 
-                    </span>
-                    <span>
-                        {order.order_request?.first_name} {order.order_request?.last_name}
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        جوال المستلم: 
-                    </span>
-                    <span dir="ltr">
-                        {order.order_request?.phone}
-                    </span>
-                </div>
+                <article>
+                    <h2>
+                        معلومات البائع
+                    </h2>
+                    <div>
+                        {order.product.seller_id}
+                    </div>
+                </article>
+                <article>
+                    <h2>
+                        معلومات المشتري
+                    </h2>
+                    <div>
+                        {order.order_request?.user_id}
+                    </div>
+                </article>
+                <article>
+                    <h2>
+                        معلومات الشحن
+                    </h2>
+
+                    {!shipped ? <p>لم يتم الشحن.</p> :
+                        <>
+                            <div>
+                                <span>
+                                    شركة الشحن:
+                                </span>
+                                <span>
+                                    {order.shipping_company}
+                                </span>
+                            </div>
+                            <div>
+                                <span>
+                                    رقم الشحنة:
+                                </span>
+                                <span>
+                                    {order.shipment_number}
+                                </span>
+                            </div>
+                        </>
+                    }
+                    <div>
+                        <span>
+                            العنوان:
+                        </span>
+                        <span>
+                            {order.order_request && addressToText(order.order_request)}
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            اسم المستلم:
+                        </span>
+                        <span>
+                            {order.order_request?.first_name} {order.order_request?.last_name}
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            جوال المستلم:
+                        </span>
+                        <span dir="ltr">
+                            {order.order_request?.phone}
+                        </span>
+                    </div>
                 </article>
             </div>
-            <p>تأكد من إيصال المنتج إلى العميل, ثم قم بتحويل المبلغ إلى البائع.</p>
+            {cancelled && <p>تم إلغاء الطلب, حالة الطلب: {(order.status)}</p>}
+            {order.status === 'pending' && <p>بانتظار الدفع.</p> }
+            {paid && !shipped && <p>بانتظار البائع لشحن النتج.</p> }
+            {shipped && <p>تأكد من إيصال المنتج إلى العميل, ثم قم بتحويل المبلغ إلى البائع.</p>}
         </div>
     );
 }
